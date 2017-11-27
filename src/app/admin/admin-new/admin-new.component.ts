20571149876d7698ca189e88ede3061b763cea91
jeffkeilman <jkeilman93@gmail.com>
2017-11-27 11:49:27 -0500

    Form validation for add new restaurant
    - Added custom form validation functionality to highlight any fields
    that are not filled in
    - Added DOMpurify to sanitize input
    - Added Noty and Animate.css for nice messaging to user

--- src/app/admin/admin-new/admin-new.component.ts
+++ src/app/admin/admin-new/admin-new.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+const Noty = require('noty');
+const createDOMPurify = require('dompurify');
+const DOMPurify = createDOMPurify(window);
 
 @Component({
   selector: 'app-admin-new',
@@ -7,16 +12,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminNewComponent implements OnInit {
 
-  newRestaurant = <any>{};
+  newRestaurant = <any>{
+    name: '',
+    address: '',
+    pic_url: '',
+    rating: '0',
+    description: '',
+    featured: false
+  };
 
-  constructor() { }
+  constructor(private router : Router) { }
 
   ngOnInit() {
   }
 
-  addRestaurant() {
-    // TODO: Actually do the thing
-    console.log(JSON.stringify(this.newRestaurant));
+  addRestaurant(myForm) {
+    const errors = [];
+    for (let key in this.newRestaurant) {
+      if (typeof(this.newRestaurant[key]) === 'string' && key !== 'rating') {
+        this.newRestaurant[key] = DOMPurify.sanitize(this.newRestaurant[key]);
+        this.newRestaurant[key] = this.newRestaurant[key].trim();
+        if (!this.newRestaurant[key]) {
+          errors.push(key);
+          document.getElementById(key).style.borderColor = "red";
+        } else {
+          document.getElementById(key).style.borderColor = "lightgrey";
+        }
+      }
+    }
+
+    if (errors.length > 0) {
+      new Noty({
+        type: 'error',
+        text: 'Please fill out all fields correctly!',
+        layout: 'topCenter',
+        animation: {
+            open: 'animated bounceInDown',
+            close: 'animated bounceOutUp'
+        },
+        timeout: 3000
+      }).show();
+    } else {
+      // do the thing
+    }
+  }
+
+  cancelAdd() {
+    this.router.navigate(["/admin"]);
   }
 
 }
